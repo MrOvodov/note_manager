08a8136617f22e36f1fdab92e56dfcd495c6d705
MrOvodov <mrovodov@gmail.com>
2025-01-12 15:23:20 +0300

    greetings.py

--- greetings.py
+++ greetings.py
@@ -1,8 +1,6 @@
-from datetime import datetime
-from datetime import date
 username, title, content, status = 'Ovodov AV','Studying at SkillPlace','Variables','Active'
-created_date=datetime.strftime(date(2025,1,10),'%d-%m-%Y')
-issue_date=datetime.strftime(date(2025,10,1),'%d-%m-%Y')
+created_date='10-01-2025'
+issue_date='01-10-2025'
 print('Имя пользователя:',username,end=';\n')
 print('Заголовок заметки:', title,end=';\n')
 print('Описание заметки:',content,end=';\n')
