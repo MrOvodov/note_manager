a2781fc6e7e94fec467571a7a5c6234a9ef93878
MrOvodov <mrovodov@gmail.com>
2025-01-14 12:23:53 +0300

    Add files via upload

---
+++ add_list.py
@@ -0,0 +1,8 @@
+title1 = input('Введите заголовок один: ')
+title2 = input('Введите заголовок два: ')
+title3 = input('Введите заголовок три: ')
+titles = []
+titles.append(title1)
+titles.append(title2)
+titles.append(title3)
+print(titles)
