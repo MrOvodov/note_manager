2be73d860c3bfdb6cf2b70b5a8cc643efbedbbcc
MrOvodov <mrovodov@gmail.com>
2025-01-16 11:13:11 +0300

    Delete greetings.py

--- greetings.py
+++
@@ -1,7 +0,0 @@
-username, title, content, status,created_date,issue_date = 'Ovodov AV','Studying at SkillPlace','Variables','Active','10-01-2025','01-10-2025'
-print('Имя пользователя:',username,end=';\n')
-print('Заголовок заметки:', title,end=';\n')
-print('Описание заметки:',content,end=';\n')
-print('Статус заметки:',status,end=';\n')
-print('Дата создания:',created_date,end=';\n')
-print('Дата истечения:',issue_date,end='.')
