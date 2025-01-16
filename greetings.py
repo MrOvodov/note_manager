a19809d0bcff0b4c0d0cf4af2488261e139767f2
MrOvodov <mrovodov@gmail.com>
2025-01-16 11:13:26 +0300

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,8 @@
+username, title, content, status,created_date,issue_date = 'Ovodov AV','Studying at SkillPlace','Variables','Active','10-01-2025','01-10-2025'
+print(f"Имя пользователя:'{username};\n"
+      f"Заголовок заметки: {title};\n"
+      f"Описание заметки:{content};\n"
+      f"Статус заметки:',{status};\n"
+      f"Дата создания:{created_date};\n"
+      f"Дата истечения:{issue_date}"
+      )
