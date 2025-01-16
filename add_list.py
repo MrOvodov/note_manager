b46639431f752c58382d97f0a8116b74379c21da
MrOvodov <mrovodov@gmail.com>
2025-01-16 10:54:28 +0300

    Delete add_list.py

--- add_list.py
+++
@@ -1,8 +0,0 @@
-title1 = input('Введите заголовок один: ')
-title2 = input('Введите заголовок два: ')
-title3 = input('Введите заголовок три: ')
-titles = []
-titles.append(title1)
-titles.append(title2)
-titles.append(title3)
-print(titles)
